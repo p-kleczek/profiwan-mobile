88569b4a87cdeea3bb6daccbb2c6d9dbe31d3bd9
Paweł Kłeczek <p.l.kleczek@gmail.com>
2014-01-05 15:42:55 +0100

    - revisions activity

--- ProfIwan-Mobile/gen/pkleczek/profiwan/R.java
+++ ProfIwan-Mobile/gen/pkleczek/profiwan/R.java
@@ -10,22 +10,45 @@ package pkleczek.profiwan;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int revisions=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int mainMenu_margin=0x7f050000;
+        public static final int revisionRow_margin=0x7f050001;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int flag_pl=0x7f020000;
+        public static final int flag_rus=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int TableLayout1=0x7f090003;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int editText1=0x7f090009;
+        public static final int imageView1=0x7f090005;
+        public static final int imageView2=0x7f090008;
+        public static final int menu_settings=0x7f09000b;
+        public static final int tableRow1=0x7f090004;
+        public static final int tableRow2=0x7f090007;
+        public static final int tableRow3=0x7f09000a;
+        public static final int textView1=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_revisions=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f080000;
+        public static final int activity_revisions=0x7f080001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
-        public static final int menu_settings=0x7f040001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int menu_settings=0x7f060001;
+        public static final int title_activity_revisions=0x7f060003;
     }
     public static final class style {
         /** 
@@ -38,10 +61,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- ProfIwan-Mobile/src/pkleczek/profiwan/MainActivity.java
+++ ProfIwan-Mobile/src/pkleczek/profiwan/MainActivity.java
@@ -2,7 +2,9 @@ package pkleczek.profiwan;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +21,8 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void startRevisions(View view) {
+		Intent intent = new Intent(this, RevisionsActivity.class);
+		startActivity(intent);
+	}
 }
